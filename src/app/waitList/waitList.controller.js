91cd8a075bd113053d78ef7feae909ef6266b0b3
Gordon Zhu <gordonmzhu@gmail.com>
2015-09-24 15:16:02 -0700

    Using ng-model and creating new Party objects.

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -11,11 +11,22 @@
     var vm = this;
 
     var fireParties = new Firebase('https://waitandeat-v2-demo.firebaseio.com/parties');
+
+    function Party() {
+      this.name = '';
+      this.phone = '';
+      this.size = '';
+      this.done = false;
+      this.notified = false;
+    }
+
+    vm.newParty = new Party();
     vm.parties  = $firebaseArray(fireParties);
     vm.addParty = addParty;
 
     function addParty() {
-      vm.parties.$add('another');
+      vm.parties.$add(vm.newParty);
+      vm.newParty = new Party();
     }
   }
 
