7ce45457e1a8a7ad5dc64833358c99d31b97a01c
Gordon Zhu <gordonmzhu@gmail.com>
2015-12-02 15:48:36 -0800

    Added barebones registration.

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,6 +5,23 @@
     .module('app.auth')
     .controller('AuthController', AuthController);
 
-  function AuthController() {}
+  AuthController.$inject = ['$firebaseAuth'];
+
+  function AuthController($firebaseAuth) {
+    var vm = this;
+    var firebaseReference = new Firebase('https://waitandeat-v2-demo.firebaseio.com/');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    vm.user = {
+      email: '',
+      password: ''
+    };
+
+    vm.register = register;
+
+    function register(user) {
+      return firebaseAuthObject.$createUser(user);
+    }
+  }
 
 })();
