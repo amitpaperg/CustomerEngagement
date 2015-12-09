cce79b692fc1bb9cc305ea1842192927d211d782
Gordon Zhu <gordonmzhu@gmail.com>
2015-12-09 12:16:56 -0800

    Used partyService.getPartiesByUser in WaitlistController.

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -10,11 +10,8 @@
   function WaitListController(partyService, textMessageService, user) {
     var vm = this;
 
-    // Log user to the console.
-    console.log(user);
-
     vm.newParty = new partyService.Party();
-    vm.parties  = partyService.parties;
+    vm.parties  = partyService.getPartiesByUser(user.uid);
     vm.addParty = addParty;
     vm.removeParty = removeParty;
     vm.sendTextMessage = sendTextMessage;
