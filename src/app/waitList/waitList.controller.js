90ba4e3480e9d9ca9408fd10e5d4ee12b14ad37a
Gordon Zhu <gordonmzhu@gmail.com>
2015-12-13 17:04:36 -0800

    Used gzPartyTable in waitlist.html.

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -5,28 +5,12 @@
     .module('app.waitList')
     .controller('WaitListController', WaitListController);
 
-  WaitListController.$inject = ['partyService', 'textMessageService', 'user'];
+  WaitListController.$inject = ['partyService', 'user'];
 
-  function WaitListController(partyService, textMessageService, user) {
+  function WaitListController(partyService, user) {
     var vm = this;
 
     vm.parties  = partyService.getPartiesByUser(user.uid);
-    vm.removeParty = removeParty;
-    vm.sendTextMessage = sendTextMessage;
-    vm.toggleDone = toggleDone;
-
-    function removeParty(party) {
-      vm.parties.$remove(party);
-    }
-
-    function sendTextMessage(party) {
-      textMessageService.sendTextMessage(party, vm.parties);
-    }
-
-    function toggleDone(party) {
-      vm.parties.$save(party);
-    }
-
   }
 
 })();
